61d8392c8886928947538da8ed130ac04eb16c09
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-01-26 21:40:15 -0300

    first commit

---
+++ src/setup.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+let path = '.env.test';
+
+if (process.env.NODE_ENV === 'production') {
+    path = '.env';
+}
+if (process.env.NODE_ENV === 'development') {
+    path = '.env.development';
+}
+
+dotenv.config({ path });
