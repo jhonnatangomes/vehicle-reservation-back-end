8dc05640dd8a51089ba0ae5e5bc54b470124a610
Jhonnatan Gomes <jhonnatangomes05@gmail.com>
2022-01-26 21:44:58 -0300

    added ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+import "./src/setup";
+
+export default {
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    migrationsTableName: "migrations",
+    entities: ["dist/entities/*.js"],
+    migrations: ["dist/migrations/*.js"],
+    cli: {
+        migrationsDir: "src/migrations",
+        entitiesDir: "dist/entities/*.js",
+    },
+    ssl:
+        process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+};
--- src/setup.ts
+++ src/setup.ts
@@ -1,12 +1,12 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-let path = '.env.test';
+let path = ".env.test";
 
-if (process.env.NODE_ENV === 'production') {
-    path = '.env';
+if (process.env.NODE_ENV === "production") {
+    path = ".env.production";
 }
-if (process.env.NODE_ENV === 'development') {
-    path = '.env.development';
+if (process.env.NODE_ENV === "development") {
+    path = ".env.development";
 }
 
 dotenv.config({ path });
